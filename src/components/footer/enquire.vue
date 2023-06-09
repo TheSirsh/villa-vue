6e0e50c756732908a2b7834df2a903e616b84ba9
TheSirsh <sirsh@bk.ru>
2023-06-09 14:41:31 +0300

    upd: add footer popup

--- src/components/footer/enquire.vue
+++ src/components/footer/enquire.vue
@@ -1,15 +1,45 @@
 <template>
     <form action="">
       <h2>Enquire</h2>
-      <input type="text" placeholder="Full Name" class="form-text">
-      <input type="email" placeholder="E-Mail" class="form-text">
-      <input type="submit" value="SUBMIT" class="form-button">
+      <input type="text" placeholder="Full Name" class="form-text" v-model="name" required>
+      <input type="email" placeholder="E-Mail" class="form-text" v-model="email" required>
+      <input type="submit" value="SUBMIT" class="form-button" @click="openEnquirePopup" >
     </form>
+    <div class="footer_popup-bg deactive" @click="closeEnquirePopup">
+      <div class="footer_popup deactive">
+        <h3>Check Your Data</h3>
+        <p>Your Name: {{ this.name }}</p>
+        <p>Your E-Mail: {{ this.email }}</p>
+        <button @click="closeEnquirePopup"
+        >OK</button>
+      </div>
+    </div>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        name: "",
+        email: "",
+      }
+    },
+    mounted() {
+        let popupBg = document.querySelector(".footer_popup-bg");
+        let popup = document.querySelector(".footer_popup");
+        this.popupBg = popupBg;
+        this.popup = popup;
+    },
+    methods: {
+      openEnquirePopup() {
+        this.popupBg.className = "footer_popup-bg active";
+        this.popup.className = "footer_popup active";
+      },
+      closeEnquirePopup() {
+        this.popupBg.className = "footer_popup-bg deactive";
+        this.popup.className = "footer_popup deactive";
+      }
+    }
   }
 </script>
 
@@ -55,5 +85,45 @@
     cursor: pointer;
     text-decoration: underline;
   }
+
+  .footer_popup-bg {
+    top: 50%;
+    left: 50%;
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0,0,0,0.5);
+    transition: 0.5s all;
+    cursor: pointer ;
+}  
+
+.footer_popup {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) scale(0);
+    background: #fff;
+    width: calc(100vw / 3.75);
+    padding: calc(100vw / 60);
+    transition: 0.5s all;
+    color: #0a0a0a; 
+}
+
+.deactive {
+  opacity: 0;
+  pointer-events: none;
+}
+
+.active {
+    transform: translate(-50%, -50%) scale(1);
+    pointer-events: all;
+    transition: 0.5s all;
+}
+
+button {
+  width: calc(100vw / 9.93);
+  height: calc(100vw / 41.67);
+  background-color: #ffffff;
+}
 
 </style>
