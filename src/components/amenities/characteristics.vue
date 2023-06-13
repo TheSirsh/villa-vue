58c7cd74b692f9f7d02914d677d7bd5f61956317
TheSirsh <sirsh@bk.ru>
2023-06-13 15:57:18 +0300

    upd: add amenities section

--- src/components/amenities/characteristics.vue
+++ src/components/amenities/characteristics.vue
@@ -1,35 +1,26 @@
 <template>
   <div class="characteristics">
-    <div class="characteristics-container">
-      <p>Swimming Pool</p>
-      <p class="characteristics-container_number">45 Sq Ft</p>
-    </div>
-    <div class="characteristics-container">
-      <p>Large Play Ground</p>
-      <p class="characteristics-container_number">80 Sq Ft</p>
-    </div>
-    <div class="characteristics-container">
-      <p>High Security</p>
-      <p class="characteristics-container_number">24/7</p>
-    </div>
-    <div class="characteristics-container">
-      <p>Water Storage</p>
-      <p class="characteristics-container_number">1200 gal</p>
-    </div>
-    <div class="characteristics-container">
-      <p>Basement</p>
-      <p class="characteristics-container_number">2</p>
-    </div>
-    <div class="characteristics-container">
-      <p>Elevator</p>
-      <p class="characteristics-container_number">2</p>
+    <div class="characteristics-container" v-for="characteristic in characteristics">
+      <p>{{ characteristic.name }}</p>
+      <p class="characteristics-container_number">{{ characteristic.data }}</p>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        characteristics: [
+          {name: "Swimming Pool", data: "45 Sq Ft", },
+          {name: "Large Play Ground", data: "80 Sq Ft", },
+          {name: "High Security", data: "24/7", },
+          {name: "Water Storage", data: "1200 gal", },
+          {name: "Basement", data: "2", },
+          {name: "Elevator", data: "2", },
+        ]
+      }
+    }
   }
 </script>
 
